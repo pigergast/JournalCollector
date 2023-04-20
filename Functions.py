995febbc1a0df3ca9959e06569e2472ee9ed0d41
Hieu <s3877375@rmit.edu.vn>
2023-04-20 20:26:05 +0700

    Added some scraping code for downloading

--- Functions.py
+++ Functions.py
@@ -4,5 +4,17 @@ import requests
 PAGEURL = 'https://libgen.is/scimag/?q='
 def getPage(DOI):
     page = requests.get(f"{PAGEURL}{DOI}")
-    print(page.content)
+    soup = BeautifulSoup(page.text, "html.parser")
+    for li in  soup.find_all('ul', {"class" : "record_mirrors" }):
+        for a in li.find_all('a'):
+            print(a['href'])
+            getDownloadLink(a['href'])
     return page
+
+def getDownloadLink(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    links = soup.find_all("a", string="GET")
+    download_links = {link.string: link["href"] for link in links}
+    print(download_links)
+    return download_links
