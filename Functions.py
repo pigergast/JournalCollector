67be05ba55d35745dc4251c64d3d8cbd5844c9f5
Hieu <vip12345k@gmail.com>
2023-06-01 14:40:32 +0700

    Revert "add get doi numbers"
    This reverts commit 142ce7d5389998f7c31533966aafed234c235c9a.

--- Functions.py
+++ Functions.py
@@ -1,41 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
-from pymed import PubMed
-
+import urllib.parse
+import re
 PAGEURL = 'https://libgen.is/scimag/?q='
-def getPage(DOI):
+def getPDF(DOI):
     page = requests.get(f"{PAGEURL}{DOI}")
     soup = BeautifulSoup(page.text, "html.parser")
     downloadList = []
     mirrors = soup.find_all('ul', {"class" : "record_mirrors" })
     if(len(mirrors) != 1):
-        return None
+        return False
+    safeDOI = urllib.parse.quote(DOI, safe='')
+    title = soup.find('a', href=f'/scimag/{safeDOI}').text
+    print(title)
     for li in mirrors:
         for a in li.find_all('a'):
             link = a['href']
             if(link.startswith('http://library.lol')):
                 downloadList.append(getDownloadLink(a['href']))
-    return page
+    #Go through each link in the download list and attempt to download. If download is successful, return true, else return false. If error occur, print exception
+    for link in downloadList:
+        try:
+            downloadPdf(link, f"{title}.pdf")
+            return True
+        except Exception as e:
+            print(e)
+            continue
+    return False
 
 def getDownloadLink(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
     links = soup.find_all("a", string="GET")
     download_links = {link.string: link["href"] for link in links}
-    print(download_links['GET'])
     return download_links['GET']
 
-
-def getListOfDoiByYear(year):
-
-    ISSN = "1527-6546"  # Journal of Nursing Scholarship ISSN
-    query = f'("{year}/1/1"[Date - Publication] : "{year}/12/31"[Date - Publication]) AND {ISSN}[IS]'
-    pubmed = PubMed(tool="MyTool", email="tangkwo1@hotmail.com")
-    results = pubmed.query(query, max_results=9999)
-
-    doi_list = []
-    for article in results:
-        if article.doi:
-            doi_list.append(article.doi)
-
-    return doi_list
+def downloadPdf(url, title):
+    response = requests.get(url)
+    print(title)
+    fileTitle = re.sub(r'[\\/*?:"<>|]', "", title)
+    file = open(fileTitle , "wb")
+    file.write(response.content)
+    file.close()
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import Functions
-
+import requests
 if __name__ == '__main__':
-    year = "2021"  # Example year
-    dois = Functions.getListOfDoiByYear(year)
-    print(dois)
-    print(len(dois))
-    Functions.getPage('10.1111/jnu.12551')
+    print(Functions.getPDF('10.1111/jnu.12551'))
 
