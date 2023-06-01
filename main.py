5bbca2f34b3b6655bb2c5ad0c06199d0e024a059
andtr-2021 <s3878698@rmit.edu.vn>
2023-06-01 15:22:43 +0700

    add summary report and downloaded articles

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import Functions
 import requests
+import csv
+
+
 if __name__ == '__main__':
-    year = input("Enter the chosen year: ")  # e.g: 2021
+    year = input("Enter the chosen year: ")  # e.g: 2016
     dois = Functions.getListOfDoiByYear(year)
-    print(dois)
-    print(Functions.getPDF('10.1111/jnu.12551'))
+    print("Number of DOIs found: " + str(len(dois)))
+
+    with open('summary.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['DOI', 'Status'])
+
+        # loop through the first five dois and download the pdfs
+        i = 0
+
+        for doi in dois:
+
+            if i == 5:
+                break
+
+            i += 1
+
+            if Functions.getPDF(doi):
+                writer.writerow([doi, 'Success'])
+            else:
+                writer.writerow([doi, 'Failed'])
+
+
 
