c7fa265e4b1dd3ac853569cac9d4c12d641fd280
andtr-2021 <s3878698@rmit.edu.vn>
2023-06-30 21:06:34 +0700

    add get pmcid and doi from pmid

--- Functions2.py
+++ Functions2.py
@@ -35,3 +35,36 @@ def get_pmids_from_journal(journal_name, start_date, end_date):
 
     pmids = data["esearchresult"].get("idlist", [])
     return pmids
+
+
+def get_pmcid_and_doi_from_pmid(pmid):
+    base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+    params = {
+        "db": "pubmed",
+        "id": pmid,
+        "retmode": "xml"
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()  # Raise an exception for non-2xx status codes
+        xml_data = response.text
+
+        pmcid = None
+        doi = None
+
+        if "<ArticleId IdType=\"pmc\">" in xml_data:
+            pmcid_start_index = xml_data.index("<ArticleId IdType=\"pmc\">") + len("<ArticleId IdType=\"pmc\">")
+            pmcid_end_index = xml_data.index("</ArticleId>", pmcid_start_index)
+            pmcid = xml_data[pmcid_start_index:pmcid_end_index]
+
+        if "<ArticleId IdType=\"doi\">" in xml_data:
+            doi_start_index = xml_data.index("<ArticleId IdType=\"doi\">") + len("<ArticleId IdType=\"doi\">")
+            doi_end_index = xml_data.index("</ArticleId>", doi_start_index)
+            doi = xml_data[doi_start_index:doi_end_index]
+
+        return pmcid, doi
+    except requests.exceptions.RequestException as e:
+        print("Error occurred:", str(e))
+
+    return None, None
