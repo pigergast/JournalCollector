febf669683b45f4ace871b5e96cc3ee9f3c1fb30
andtr-2021 <s3878698@rmit.edu.vn>
2023-07-05 17:32:45 +0700

    add pmcid

--- Functions2.py
+++ Functions2.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+from urllib.parse import quote
 from bs4 import BeautifulSoup
 
 
@@ -16,3 +17,64 @@ def extract_journal_issn(csv_file):
 
     return journal_list
 
+
+def get_journal_pmcids(issn, start_date, end_date):
+    base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+    parameters = {
+        "db": "pmc",
+        "term": f'"{issn}"[jour] AND open access[filter] AND ("{start_date}"[PubDate] : "{end_date}"[PubDate])',
+        "retmode": "json",
+        "retmax": 100000,  # Adjust the value based on your requirements
+        "usehistory": "y"
+    }
+
+    response = requests.get(base_url, params=parameters)
+    response_json = response.json()
+
+    if 'esearchresult' not in response_json or 'idlist' not in response_json['esearchresult']:
+        print("Error: Invalid API response")
+        return []
+
+    webenv = response_json['esearchresult']['webenv']
+    query_key = response_json['esearchresult']['querykey']
+
+    pmcid_list = []
+    retstart = 0
+    retmax = 10000  # Adjust the value based on your requirements
+
+    while retstart <= int(response_json['esearchresult']['count']):
+        fetch_parameters = {
+            "db": "pmc",
+            "query_key": query_key,
+            "WebEnv": webenv,
+            "retmode": "json",
+            "retstart": retstart,
+            "retmax": retmax
+        }
+
+        fetch_response = requests.get(base_url, params=fetch_parameters)
+        fetch_json = fetch_response.json()
+
+        if 'esearchresult' in fetch_json and 'idlist' in fetch_json['esearchresult']:
+            pmcid_list.extend(fetch_json['esearchresult']['idlist'])
+        else:
+            print("Error: Invalid fetch response of ", issn)
+
+
+        retstart += retmax
+
+    return pmcid_list
+
+
+def write_pmcids_to_csv(array):
+    # Prepare the data to be written
+    data = [{'pmcid': item} for item in array]
+
+    # Define the fieldnames (column names) for the CSV file
+    fieldnames = ['pmcid']
+
+    # Write the data to the CSV file
+    with open('metadata.csv', 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
--- main2.py
+++ main2.py
@@ -4,3 +4,19 @@ csv_file = 'journal-list.csv'
 journal_list = Functions2.extract_journal_issn(csv_file)
 print("The number of journals in the list:", len(journal_list))
 print("The first 10 journals:", journal_list[:10])
+
+start_date = '2022/10/30'
+end_date = '2023/07/01'
+
+pmcid_list = []
+progress = 0
+for journal in journal_list:
+    progress += 1
+    pmcid_list.extend(Functions2.get_journal_pmcids(journal, start_date, end_date))
+    if progress % 10 == 0:
+        print(f"Progress: {progress}/{len(journal_list)}")
+
+Functions2.write_pmcids_to_csv(pmcid_list)
+
+print("The number of PMCIDs found from the PMC Open Access Subset:", len(pmcid_list))
+print("The first 10 PMCIDs:", pmcid_list[:10])
