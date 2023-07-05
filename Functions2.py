f3f5e9a8779ecbada3dc63cde5430e62fc547f3e
andtr-2021 <s3878698@rmit.edu.vn>
2023-07-05 16:48:09 +0700

    find pmcid by issn

--- Functions2.py
+++ Functions2.py
@@ -3,7 +3,7 @@ import csv
 from bs4 import BeautifulSoup
 
 
-def extract_journal_names(csv_file):
+def extract_journal_issn(csv_file):
     journal_list = []
 
     with open(csv_file, newline='') as file:
@@ -11,94 +11,8 @@ def extract_journal_names(csv_file):
         next(reader)  # Skip the header row
 
         for row in reader:
-            journal_name = row[0]
+            journal_name = row[2]
             journal_list.append(journal_name)
 
     return journal_list
-
-
-def get_pmids_from_journal(journal_name, start_date, end_date):
-    base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
-    query = f"{journal_name} AND ({start_date}[Date - Publication] : {end_date}[Date - Publication]) AND Free Full " \
-            f"Text[filter]"
-    params = {
-        "db": "pubmed",
-        "term": query,
-        "retmax": 100000,  # Adjust as per your requirement (maximum is 100,000)
-        "retmode": "json"
-    }
-
-    response = requests.get(base_url, params=params)
-    data = response.json()
-
-    if "error" in data:
-        print(f"Error: {data['error']}")
-        return []
-
-    pmids = data["esearchresult"].get("idlist", [])
-    return pmids
-
-
-def get_pmcid(pmid):
-    url = f"https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?ids={pmid}"
-
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for non-2xx status codes
-        soup = BeautifulSoup(response.content, "lxml-xml")
-
-        records = soup.find_all("record")
-        if len(records) > 0:
-            record = records[0]
-            pmcid = record.get("pmcid")
-            return pmcid
-    except requests.exceptions.RequestException as e:
-        print("Error occurred:", str(e))
-
-    return None
-
-
-def write_array_to_csv(array):
-    # Prepare the data to be written
-    data = [{'pmid': item} for item in array]
-
-    # Define the fieldnames (column names) for the CSV file
-    fieldnames = ['pmid']
-
-    # Write the data to the CSV file
-    with open('metadata.csv', 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(data)
-
-
-def extract_pmids_from_csv(filename):
-    pmid_list = []
-
-    with open(filename, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            pmid = row['pmid']
-            pmid_list.append(pmid)
-
-    return pmid_list
-
-
-def write_pmcid_to_csv(pmcid_list):
-    with open('metadata.csv', 'r', newline='') as file:
-        reader = csv.DictReader(file)
-        fieldnames = reader.fieldnames + ['pmcid']
-
-        rows = []
-        for row in reader:
-            pmcid = pmcid_list.pop(0) if pmcid_list else ''
-            row['pmcid'] = pmcid
-            rows.append(row)
-
-    with open('metadata.csv', 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(rows)
-
-
 
--- main2.py
+++ main2.py
@@ -1,40 +1,6 @@
 import Functions2
-from multiprocessing import Pool
 
 csv_file = 'journal-list.csv'
-journal_list = Functions2.extract_journal_names(csv_file)
+journal_list = Functions2.extract_journal_issn(csv_file)
 print("The number of journals in the list:", len(journal_list))
-"""
-start_date = "2022/10/30"
-end_date = "2023/07/01"
-
-pmid_list = []
-progress = 0
-for journal_name in journal_list:
-    progress += 1
-    pmids = Functions2.get_pmids_from_journal(journal_name, start_date, end_date)
-    pmid_list.extend(pmids)
-    if progress % 10 == 0:
-        print(f"Progress: {progress}/{len(journal_list)}")
-
-print("The number of PMIDs found:", len(pmid_list))
-print("The first 10 PMIDs:", pmid_list[:10])
-
-Functions2.write_array_to_csv(pmid_list)
-"""
-pmid_list = Functions2.extract_pmids_from_csv('metadata.csv')
-print("The number of PMIDs found:", len(pmid_list))
-
-pmcid_list = []
-progress2 = 0
-for pmid in pmid_list:
-    progress2 += 1
-    pmcid = Functions2.get_pmcid(pmid)
-    if pmcid:
-        print(pmcid)
-        pmcid_list.append(pmcid)
-    if progress2 % 100 == 0:
-        print(f"Progress: {progress2}/{len(pmid_list)}")
-
-
-Functions2.write_pmcid_to_csv(pmcid_list)
+print("The first 10 journals:", journal_list[:10])
