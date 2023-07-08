41bb6025919c1098e4e91db116c7ae9ed51db63a
andtr-2021 <s3878698@rmit.edu.vn>
2023-07-08 12:41:26 +0700

    update journal-lis-report

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
 import Functions2
 
 csv_file = 'journal-list.csv'
-name_list = Functions2.extract_journal_list_col(csv_file, 1)
+name_list = Functions2.extract_journal_list_col(csv_file, 0)
 issn_list = Functions2.extract_journal_list_col(csv_file, 2)
 print("The first three journal names:", name_list[:3])
 print("The first three ISSNs:", issn_list[:3])
