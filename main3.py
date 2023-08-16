86e0a426bf17d34d5f95c0df58a345130e39a45e
andtr-2021 <s3878698@rmit.edu.vn>
2023-08-16 19:53:39 +0700

    create master list 2

--- main3.py
+++ main3.py
@@ -46,9 +46,15 @@ if __name__ == '__main__':
 
     CSVFunctions.create_master_list(issn_list, pmid_list, doi_list, status_list, pmcid_list, 'master-list2.csv')
     """
+    issn_list = CSVFunctions.extract_col_from_csv('master-list.csv', 0)
     pmid_list = CSVFunctions.extract_col_from_csv('master-list.csv', 1)
-    print(len(pmid_list))
-    print(pmid_list[8654])
+    doi_list = CSVFunctions.extract_col_from_csv('master-list.csv', 2)
+    status_list = CSVFunctions.extract_col_from_csv('master-list.csv', 3)
+    pmcid_list = CSVFunctions.extract_col_from_csv('pmcid-list.csv', 0)
+
+    CSVFunctions.create_master_list(issn_list, pmid_list, doi_list, status_list, pmcid_list, 'master-list2.csv')
+
+
 
 
 
