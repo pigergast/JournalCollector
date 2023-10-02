97f2fbbbfeffad925bc2db4598132dcb66228840
andtr-2021 <an.dtruongan@gmail.com>
2023-10-02 14:34:16 +0700

    update main4

--- main4.py
+++ main4.py
@@ -6,24 +6,27 @@ if __name__ == '__main__':
     doi_list = CSVFunctions.extract_col_from_csv('master-list.csv', 2)
 
     libGenList = []
-    progress = 12372
+    progress = 0
 
     # start from 12373 to the end
-    for doi in doi_list[12373:]:
+    for doi in doi_list:
         progress += 1
         print("Progress:", progress - 1, "out of", len(doi_list), "| DOI:", doi)
         temp_check = checkLibgen(doi)
         if temp_check is not None:
-            libGenList.append(temp_check)
-            CSVFunctions.add_single_item_to_csv(temp_check, 'libgen-list4.csv', 'LibGen')
+            libGenList.append(temp_check[0])
+            CSVFunctions.add_single_item_to_csv(temp_check[0], 'libgen-list5.csv', 'LibGen')
             print(temp_check)
         else:
             libGenList.append('None')
-            CSVFunctions.add_single_item_to_csv('None', 'libgen-list4.csv', 'LibGen')
+            CSVFunctions.add_single_item_to_csv('None', 'libgen-list5.csv', 'LibGen')
             print('None')
 
     print("The length of LibGen list:", len(libGenList))
 
 
 
-    # print(checkLibgen('10.2174/0929867324666170801101448'))
+
+
+
+
