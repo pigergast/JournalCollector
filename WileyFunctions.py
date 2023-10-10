29a7ad96fa43e28782d427bcd3c55f1cbe1ffeee
Hieu <s3877375@rmit.edu.vn>
2023-10-10 13:22:32 +0700

    function

---
+++ WileyFunctions.py
@@ -0,0 +1,18 @@
+import urllib
+
+import requests
+
+
+def checkWiley(inputDoi):
+    token = "7787c853-8908-4946-8107-050bcf22971a"
+    apiUrl = "https://api.wiley.com/onlinelibrary/tdm/v1/articles/"
+    headers = {'Wiley-TDM-Client-Token': token}
+    doi = inputDoi
+    # encode doi
+    doi = urllib.parse.quote(doi, safe='')
+    print(doi)
+    response = requests.get(f"{apiUrl}{doi}", headers=headers)
+    if response.status_code == 200:
+        return True
+    else:
+        return False
